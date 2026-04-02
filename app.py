599a77327effa39f3a2bd573edeafc1f99dfeb4a
Lynn578 <wambuievalyn87@gmail.com>
2026-04-02 10:51:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_title="Food Price Predictor - Kenya", 
     page_icon="🍽️", 
     layout="wide",
-    color: white;
+    
 )
 
 # ============================================
